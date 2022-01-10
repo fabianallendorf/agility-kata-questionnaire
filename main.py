e334a4e4ff23aa0e0c2708b2df9f076e22c2ac5d
Malte Viefhues <malte.viefhues@node.energy>
2022-01-10 11:46:21 +0100

    working ui

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from importlib import resources
+
+import test.files
 from src.file_reader import read_file
 from src.questionnaire_parser import QuestionnaireParser
+from src.ui import QuestionnaireUI
 
 
 def main():
-    questionnaire_lines = read_file()
-    QuestionnaireParser.parse_questionnaire(questionnaire_lines=questionnaire_lines)
+    with resources.path(test.files, test.files.WITH_MULTIPLE_QUESTIONS) as path:
+        questionnaire_lines = read_file(path)
+    parser = QuestionnaireParser()
+    questions = parser.parse_questionnaire(questionnaire_lines=questionnaire_lines)
+
+    ui = QuestionnaireUI()
+    ui.display_questionnaire(questions)
+
+    ui.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- src/ui.py
+++ src/ui.py
@@ -1,30 +1,47 @@
 import random
 import tkinter as tk
-from itertools import chain
 from tkinter import ttk
 
 from src.dataclasses import Question
+from src.questionnaire_validator import QuestionnaireValidator
 
 
 class QuestionnaireUI(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master=master)
         self.grid()
+        self.check_vars = dict()
 
     def display_questionnaire(self, questions: list[Question]):
         row = 0
         for question in questions:
-            ttk.Label(master=self, text=question.text).grid(column=0, row=row)
+            ttk.Label(master=self, text="\n").grid(column=1, row=row)
+            row += 1
+            ttk.Label(master=self, text=question.text).grid(column=1, row=row)
+            row += 1
             answers = question.correct_answers + question.incorrect_answers
             random.shuffle(answers)
             for answer in answers:
+                self.check_vars[answer] = tk.IntVar()
+                ttk.Checkbutton(master=self, text=answer.text, variable=self.check_vars[answer]).grid(column=1, row=row)
                 row += 1
-                ttk.Checkbutton(master=self, text=answer.text).grid(column=0, row=row)
-            row += 1
-
+        ttk.Label(master=self, text="\n").grid(column=1, row=row)
+        row += 1
         ttk.Button(master=self, text="Show results", command=self.show_results).grid(
             column=0, row=row
         )
-        ttk.Button(master=self, text="Quit", command=self.destroy).grid(
-            column=0, row=row
-        )
+        ttk.Button(master=self, text="Quit", command=self.quit).grid(
+            column=2, row=row
+        )  # self.destroy ?
+
+    def show_results(self):
+        selected_answers = self._collect_selected_answers()
+        print(selected_answers)
+
+    def _collect_selected_answers(self):
+        selected_answers = []
+        for answer, var in self.check_vars.items():
+            is_selected = var.get()
+            if is_selected:
+                selected_answers.append(answer)
+        return selected_answers
